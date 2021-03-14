4b772113855ddc776de90ae610b12ca5210a5edd
duva <wanjiakun.zhu@gmail.com>
2021-03-14 14:33:33 -0400

    add animations

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ message.anchor.y = 0.5;
 message.x = window.innerWidth / 2;
 message.y = window.innerHeight / 2;
 
+let fires = [];
 let cat;
 function setup(x, y) {
   //Create the cat sprite
@@ -42,10 +43,13 @@ function setup(x, y) {
   cat.anchor.y = 0.5;
   cat.x = x;
   cat.y = y;
+  fires.push(cat);
   app.stage.addChild(cat);
   app.render();
 }
 
+let cosBase = 0;
+console.log(Math.cos(2 * Math.PI), Math.cos(0));
 function App() {
   const [text, setText] = useState("Some cool text");
   useEffect(() => {
@@ -53,13 +57,23 @@ function App() {
     document.body.appendChild(app.view);
     background.beginFill(0x5fddff);
     background.drawRect(0, 0, 256, 256);
-    // background.interactive = true;
-    // background.mousedown = (e) => {
-    //   console.log("background", e);
-    //   setup(20, 20);
-    // };
     app.stage.addChild(background);
     app.stage.addChild(message);
+
+    app.ticker.add((delta) => {
+      // just for fun, let's rotate mr rabbit a little
+      // delta is 1 if running at 100% performance
+      // creates frame-independent transformation
+      //console.log(Math.cos(delta));
+      cosBase += 0.01 * delta;
+      if (cosBase > 2 * Math.PI) cosBase = 0;
+      message.rotation = (Math.cos(cosBase) * Math.PI) / 4;
+      for (let fire of fires) {
+        fire.rotation = (Math.cos(cosBase) * Math.PI) / 4;
+      }
+      // console.log(Math.cos(cosBase));
+      // message.rotation += 0.1 * delta;
+    });
   }, []);
 
   message.text = text;
